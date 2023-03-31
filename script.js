3872ffc4cbe203733bb3f3c3d1bde8ea273729ec
joao <joaorodrigues.009@outlook.com>
2023-03-31 09:36:59 -0300

    aula java 30-03-2023

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
-//substituindo palavras
+// SUBSTITUINDO PALAVRAS            
 
-let txt= "Estão chegando as provas"
-console.log(txt.replace("provas", "avaliações"));
+//let txt ="Estão chegando as provas"
+//console.log(txt.replace("provas","avaliações"));
 
-//apresentando valores depois vírgula
+// APRESENTANDO SOMENTE VALORES DEPOIS DA VIRGULA
 
-let num = 123.5256
-document.write(num.toFixed(2))
-document.write("<br><br>")
+//let num = 123.5256
+//document.write(num.toFixed(2));
+//document.write("<br><br>")
+
+//let num2 = 123.5256
+//document.write(num.toPrecision(4))
+//document.write("<br><br>")
 
 
-let num2 = 123.5256
-document.write(num2.toPrecision(4))
-document.write("<br><br>")
+//CAIXA DE ALERTA
 
+//alert("olá mundo")
 
-//caixa de alerta
+// CAIXA DE CONFIRMAÇÃO
 
-//alert("Olá Mundo")
+//let teste=confirm('você é uma aluno?')
+//document.write("O resultado é: ", teste)
 
-//caixa de confirmação
+//let texto= prompt("digite seu nome")
+//document.write("O nome dele é: ",texto)
 
-//let teste=confirm("Você é aluno?")
+//IF
+//if(20 < 10){
+  //  document.write("O valor 20 é maior que 10")
+//}else{
+ //   document.write("o valor 20 não é maior que 10")
+//}
 
-//document.write("O resultado é:", teste)
+//IF ELSE DECLARANDO VARIAVEL
 
-//caixa de prompt
+//let valor = 10 
+//if (valor > 7){
+//  document.write("O valor é maior que 7")
+//}else{
+  //  document.write("O valor é menor que 7")
+//}
 
-let teste= prompt("digite seu nome")
-document.write("O nome dele é: ", teste)
+//IF EELSE ENCADEADO
 
+/*
+let idade= prompt("Digite sua idade")
+
+if(idade <= 12){
+    document.write("é uma criança")
+}
+else if (idade <=17){
+    document.write("é um adolescente")
+}
+else if (idade <=50){
+    document.write("é um adulto")
+}
+else{
+    document.write("é um idoso experiente")
+}
+*/
+/*
+let idade =prompt("digite sua idade")
+
+if(idade <= 10){
+    document.write("Kids")
+}
+else if (idade <=14){
+    document.write("matine")
+}
+else if (idade <=19){
+    document.write("noturna")
+}
+else if (idade <=20){
+    document.write("madruga")
+}
+else{
+    document.write("Sedentário")
+}
+*/
